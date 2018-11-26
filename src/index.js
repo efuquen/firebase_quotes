974a9fb64456476cb492e7965af309251f7a471c
Edwin Fuquen <efuquen@edwins-mbp.lan>
2018-11-26 03:02:30 -0500

    remove useless category

---
+++ src/index.js
@@ -0,0 +1,48 @@
+const admin = require('firebase-admin');
+var config = require('config');
+const fetch = require('node-fetch');
+
+const MS_IN_SEC = 1000;
+
+const firebaseServiceAccount = config.get('firebase').get('service_account');
+const mashapeKey = config.get('mashape').get('key');
+const quoteApiUrl = config.get('mashape').get('quote_api_url');
+
+admin.initializeApp({
+  credential: admin.credential.cert(config.util.toObject(firebaseServiceAccount)),
+  databaseURL: 'https://' + firebaseServiceAccount.get('project_id') + '.firebaseio.com'
+});
+
+const db = admin.database();
+const historyRef = db.ref('/history');
+const historyCountRef = db.ref('/history_count');
+const quoteRef = db.ref('/quote');
+
+let previousQuote = null;
+
+function getQuote() {
+  fetch(quoteApiUrl, {
+    headers: {
+      'X-Mashape-Key': mashapeKey,
+      'Content-Type': 'application/json'
+    },
+  }).then((res) => res.json())
+    .then((json) => {
+      const quoteJson = json[0];
+      delete quoteJson.category;
+      quoteRef.set(quoteJson);
+
+      if(previousQuote != null) {
+        const newHistoryRef = historyRef.push();
+        newHistoryRef.set(previousQuote);
+        historyCountRef.transaction((currentCount) => {
+          return currentCount + 1;
+        });
+      }
+
+      previousQuote = quoteJson;
+    }
+  );
+}
+
+setInterval(getQuote, config.get("quote_rotate_seconds") * MS_IN_SEC);
